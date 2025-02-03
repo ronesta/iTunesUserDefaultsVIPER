987c698c86c12bc8a9b35dcd69038403d037a732
ronesta <gvozzzd123@gmail.com>
2025-02-03 13:03:40 +0300

    Initial commit

---
+++ iTunesUserDefaultsVIPER/iTunesUserDefaultsVIPER/SearchViewController.swift
@@ -0,0 +1,137 @@
+//
+//  ViewController.swift
+//  iTunesUserDefaultsVIPER
+//
+//  Created by Ибрагим Габибли on 02.02.2025.
+//
+
+import UIKit
+import SnapKit
+
+final class SearchViewController: UIViewController {
+    let searchBar: UISearchBar = {
+        let searchBar = UISearchBar()
+        searchBar.searchBarStyle = .minimal
+        searchBar.placeholder = "Search Albums"
+        searchBar.sizeToFit()
+        return searchBar
+    }()
+
+    private let collectionView: UICollectionView = {
+        let layout = UICollectionViewFlowLayout()
+        layout.itemSize = CGSize(width: UIScreen.main.bounds.width - 15, height: 130)
+        layout.minimumLineSpacing = 7
+        layout.minimumInteritemSpacing = 0
+        layout.sectionInset = UIEdgeInsets.zero
+
+        let collectionView = UICollectionView(frame: .zero,
+                                              collectionViewLayout: layout
+        )
+        collectionView.backgroundColor = .clear
+        collectionView.register(
+            AlbumCollectionViewCell.self,
+            forCellWithReuseIdentifier: AlbumCollectionViewCell.id
+        )
+
+        return collectionView
+    }()
+
+    var albums = [Album]()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupViews()
+    }
+
+    private func setupViews() {
+        view.backgroundColor = .systemGray6
+        view.addSubview(searchBar)
+        view.addSubview(collectionView)
+        navigationItem.titleView = searchBar
+
+        searchBar.delegate = self
+        collectionView.dataSource = self
+        collectionView.delegate = self
+
+        collectionView.snp.makeConstraints { make in
+            make.top.equalTo(view.safeAreaLayoutGuide)
+            make.bottom.equalTo(view.safeAreaLayoutGuide)
+            make.horizontalEdges.equalToSuperview()
+        }
+    }
+
+    func searchAlbums(with term: String) {
+        if let savedAlbums = StorageManager.shared.loadAlbums(for: term) {
+            albums = savedAlbums
+            collectionView.reloadData()
+            return
+        }
+
+        NetworkManager.shared.fetchAlbums(albumName: term) { [weak self] result in
+            switch result {
+            case .success(let albums):
+                DispatchQueue.main.async {
+                    self?.albums = albums.sorted { $0.collectionName < $1.collectionName }
+                    self?.collectionView.reloadData()
+                    StorageManager.shared.saveAlbums(albums, for: term)
+                    print("Successfully loaded \(albums.count) albums.")
+                }
+            case .failure(let error):
+                print("Failed to load images with error: \(error.localizedDescription)")
+            }
+        }
+    }
+}
+
+// MARK: - UICollectionViewDataSource
+extension SearchViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        albums.count
+    }
+
+    func collectionView(_ collectionView: UICollectionView,
+                        cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(
+            withReuseIdentifier: AlbumCollectionViewCell.id,
+            for: indexPath)
+                as? AlbumCollectionViewCell else {
+            return UICollectionViewCell()
+        }
+
+        let album = albums[indexPath.item]
+        let urlString = album.artworkUrl100
+
+        NetworkManager.shared.loadImage(from: urlString) { loadedImage in
+            DispatchQueue.main.async {
+                guard let cell = collectionView.cellForItem(at: indexPath) as? AlbumCollectionViewCell  else {
+                    return
+                }
+                cell.configure(with: album, image: loadedImage)
+            }
+        }
+        return cell
+    }
+}
+
+// MARK: - UICollectionViewDelegate
+extension SearchViewController: UICollectionViewDelegate {
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+
+        let albumViewController = AlbumViewController()
+        let album = albums[indexPath.item]
+        albumViewController.album = album
+        navigationController?.pushViewController(albumViewController, animated: true)
+    }
+}
+
+// MARK: - UISearchBarDelegate
+extension SearchViewController: UISearchBarDelegate {
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        searchBar.resignFirstResponder()
+        guard let searchTerm = searchBar.text, !searchTerm.isEmpty else {
+            return
+        }
+        StorageManager.shared.saveSearchTerm(searchTerm)
+        searchAlbums(with: searchTerm)
+    }
+}
---
+++ iTunesUserDefaultsVIPER/iTunesUserDefaultsVIPER/StorageManager/StorageManager.swift
@@ -0,0 +1,74 @@
+//
+//  StorageManager.swift
+//  iTunesUserDefaultsVIPER
+//
+//  Created by Ибрагим Габибли on 03.02.2025.
+//
+
+import Foundation
+import UIKit
+
+final class StorageManager {
+    static let shared = StorageManager()
+    private let historyKey = "searchHistory"
+    private init() {}
+
+    func saveAlbums(_ albums: [Album], for searchTerm: String) {
+        do {
+            let data = try JSONEncoder().encode(albums)
+            UserDefaults.standard.set(data, forKey: searchTerm)
+        } catch {
+            print("Failed to encode characters: \(error)")
+        }
+    }
+
+    func saveImage(_ image: Data, key: String) {
+        UserDefaults.standard.set(image, forKey: key)
+    }
+
+    func saveSearchTerm(_ term: String) {
+        var history = getSearchHistory()
+        if !history.contains(term) {
+            history.insert(term, at: 0)
+            UserDefaults.standard.set(history, forKey: historyKey)
+        }
+    }
+
+    func loadAlbums(for searchTerm: String) -> [Album]? {
+        if let data = UserDefaults.standard.data(forKey: searchTerm) {
+            do {
+                let albums = try JSONDecoder().decode([Album].self, from: data)
+                return albums
+            } catch {
+                print("Failed to encode: \(error)")
+                return nil
+            }
+        } else {
+            return nil
+        }
+    }
+
+    func loadImage(key: String) -> Data? {
+        return UserDefaults.standard.data(forKey: key)
+    }
+
+    func getSearchHistory() -> [String] {
+        return UserDefaults.standard.array(forKey: historyKey) as? [String] ?? []
+    }
+
+    func clearAlbums() {
+        let history = getSearchHistory()
+        for term in history {
+            UserDefaults.standard.removeObject(forKey: term)
+        }
+        clearHistory()
+    }
+
+    func clearImage(key: String) {
+        UserDefaults.standard.removeObject(forKey: key)
+    }
+
+    func clearHistory() {
+        UserDefaults.standard.removeObject(forKey: historyKey)
+    }
+}
