4e5e25ceb59fac800f4e906c37b85f63e991b905
ronesta <gvozzzd123@gmail.com>
2025-04-18 21:06:22 +0300

    AlbumViewControllerSnapshotTests

---
+++ iTunesUserDefaultsVIPER/iTunesTests/iTunes/AlbumViewController/View/AlbumViewControllerSnapshotTests.swift
@@ -0,0 +1,29 @@
+//
+//  AlbumViewControllerSnapshotTests.swift
+//  iTunesUserDefaultsVIPER
+//
+//  Created by Ибрагим Габибли on 18.04.2025.
+//
+
+import XCTest
+import SnapshotTesting
+@testable import iTunesUserDefaultsVIPER
+
+final class AlbumViewControllerSnapshotTests: XCTestCase {
+    func testAppearanceWithData() {
+        let mockPresenter = MockAlbumPresenter()
+        let viewController = AlbumViewController(presenter: mockPresenter)
+
+        let image = UIImage(systemName: "checkmark.diamond")
+        let album = Album(artistId: 111051,
+                          artistName: "Eminem",
+                          collectionName: "The Eminem Show",
+                          artworkUrl100: "url_to_image",
+                          collectionPrice: 10.99
+                         )
+
+        viewController.displayAlbumDetails(album: album, image: image!)
+
+        assertSnapshot(of: viewController, as: .image)
+    }
+}
